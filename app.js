1e40f9a61edf3ec39509e2be92397bd35bf80771
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2020-05-30 02:00:18 +0530

    added controllers for the app

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//Import routes for "catalog" area of site
+var catalogRouter = require('./routes/catalog'); 
 
 var app = express();
 //Set up mongoose connection
@@ -46,6 +48,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
